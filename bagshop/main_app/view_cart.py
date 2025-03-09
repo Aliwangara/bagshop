7bc28480ec1f4b1cfb345df90287cadfeedfc1be
Ali Wangara <Aliwangara63@gmail.com>
2025-03-09 13:34:00 +0300

    changed imports in views

--- bagshop/main_app/view_cart.py
+++ bagshop/main_app/view_cart.py
@@ -1,4 +1,4 @@
-from .models import product, Invoice,InvoiceItem,AbandonedCart
+from .models import product, Invoice, InvoiceItem, AbandonedCart
 from django.shortcuts import render, get_object_or_404,redirect
 from django.core.mail import send_mail
 from django.core.mail import EmailMessage
